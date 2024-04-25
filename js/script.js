38b74afa227f1207866ebd66601c335546c9bcd8
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-04-25 10:32:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello World!');
-
-let favoriteFood = "Pommes";
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Butterfree", height: "8", types: ['psychic', 'poison']},
+    { name: "Ninetales", height: "8", types: ['dark', 'normal']},
+    { name: "Seviper", height: "8", types: ['normal', 'ghost']}
+];
