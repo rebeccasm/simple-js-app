93c1c75143be761238b5de2ff6840e8a5d2ce5f3
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-05-07 21:09:00 +0200

    add on

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 let pokemonList = [
-    { name: "Butterfree", height: "8", types: ['psychic', 'poison']},
-    { name: "Ninetales", height: "8", types: ['dark', 'normal']},
-    { name: "Seviper", height: "8", types: ['normal', 'ghost']}
+    { name: "Butterfree", height: 6, types: ['psychic', 'poison'],},
+    { name: "Ninetales", height: 10, types: ['dark', 'normal']},
+    { name: "Seviper", height: 8, types: ['normal', 'ghost']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height <10 && pokemonList[i].height >5) {
+    console.log(pokemonList[i].name + " is a Pokemon" + "<br>");
+    } else if (pokemonList[i].height <5) {
+        console.log(pokemonList[i].name + " is a small Pokemon" + "<br>");
+    } else {
+        console.log(pokemonList[i].name + " is a big Pokemon" + "<br>")
+    }
+};
+
 document.write(pokemonList);
