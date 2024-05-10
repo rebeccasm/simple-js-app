8f1065f5b82290de58b9ef041fbb0e7b2bb64e6d
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-05-10 21:23:50 +0200

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,44 @@
-let pokemonList = [
-    { name: "Butterfree", height: 6, types: ['psychic', 'poison'],},
-    { name: "Ninetales", height: 10, types: ['dark', 'normal']},
-    { name: "Seviper", height: 8, types: ['normal', 'ghost']}
-];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height <10 && pokemonList[i].height >5) {
-    console.log(pokemonList[i].name + " is a Pokemon" + "<br>");
-    } else if (pokemonList[i].height <5) {
-        console.log(pokemonList[i].name + " is a small Pokemon" + "<br>");
-    } else {
-        console.log(pokemonList[i].name + " is a big Pokemon" + "<br>")
-    }
+//  create pokemonList
+let pokemonList = [];
+
+let bulbasaur = {
+name: "Bulbasaur",
+height: 7,
+types: ["grass", "poison"]
 };
 
-document.write(pokemonList);
+let charmander = {
+name: "Charmander",
+height: 6,
+types: ["fire"]
+};
+
+let squirtle = {
+name: "Squirtle",
+height: 5,
+types: ["water"]
+};
+
+// push the pokemonList
+pokemonList.push(bulbasaur, 
+    charmander, 
+    squirtle);
+
+// Loop through each item in the pokemonList array
+for (let i = 0; i < pokemonList.length; i++) {
+// Get the current Pokemon object
+let pokemon = pokemonList[i];
+
+// Write the Pokemon's name and height to the DOM
+document.write(`${pokemon.name} (height: ${pokemon.height})`);
+
+// Check if the height is above a certain value
+if (pokemon.height > 6) {
+document.write(" - Wow, that's big!");
+}
+
+// Add a line break after each Pokemon
+document.write('\n');
+}
+// console.log("bulbasaur\ncharmander\nsquirtle");
