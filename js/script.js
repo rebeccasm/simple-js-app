64213a344493cfd649f1d005ed8b54b7d788884f
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-06-05 14:27:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const pokemonRepository = (function () {
   }
   function addListItem(pokemon) {
     let listPokemon = document.querySelector('.pokemon-list');
-    let pokemonElement = document.createElement('li');
+    let pokemonElement = document.createElement('div');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('pokemon-button');
@@ -47,15 +47,53 @@ const pokemonRepository = (function () {
         // Now we add the details to the item
         item.imgUrl = details.sprites.front_default;
         item.height = details.height;
-        console.log(item);
+        item.types = details.types;
+        item.weight = details.weight;
       })
       .catch(err => console.error('Error fetching pokemon details: ', err));
   }
   function showDetails(item) {
     loadDetails(item)
       .then(() => {
-        console.log(item);
+        showModal(item);
       });
+  }
+  function showModal(item) {
+    let modalContainer = document.querySelector('#modal-container');
+    let modal = document.querySelector('.modal');
+    let closeButton = document.querySelector('.close-button');
+    let modalTitle = document.querySelector('.modal-title');
+    let modalBody = document.querySelector('.modal-body');
+    let modalImg = document.querySelector('.modal-img');
+    
+    modalTitle.innerText = item.name;
+    modalBody.innerHTML = `
+      <img class="modal-img" src="${item.imgUrl}" alt="${item.name} image" />
+      <p class="modal-info">Height: ${item.height} </p>
+      <p class="modal-info">Weight: ${item.weight} </p>
+      <ul class="modal-info">Types:</ul>
+    `;
+    for (let i = 0; i < item.types.length; i++) {
+      let li = document.createElement('li');
+      li.innerText = item.types[i].type.name;
+      modalBody.querySelector('ul').appendChild(li);
+    }
+    modalContainer.classList.add('is-visible');
+    closeButton.addEventListener('click', hideModal);
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();
+      }
+    });
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal) {
+        hideModal();
+      }
+    });
+  }
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
   }
   return {
     add,
