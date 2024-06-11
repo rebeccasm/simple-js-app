12e2c393474fb592d5eeae7817a2ea15ee12385e
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-06-11 11:59:32 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -109,3 +109,24 @@ pokemonRepository.loadList().then(() => {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+$(document).ready(function() {
+  // Add item functionality
+  $('#add-item').on('click', function() {
+    let newItem = $('#new-item').val();
+    if (newItem !== '') {
+      $('#list').append(`<li><span class="item">${newItem}</span><span class="delete">X</span></li>`);
+      $('#new-item').val('');
+    }
+  });
+  // Cross out item functionality
+  $('#list').on('click', '.item', function() {
+    $(this).toggleClass('completed');
+  });
+  // Delete item functionality
+  $('#list').on('click', '.delete', function() {
+    $(this).parent('li').remove();
+  });
+  // Reorder items functionality
+  $('#list').sortable();
+});
