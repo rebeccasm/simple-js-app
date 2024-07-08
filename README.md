02ef511e98ada3c4284945fec1a868c64aaf628f
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-07-08 17:02:59 +0200

    Update

---
+++ README.md
@@ -0,0 +1,22 @@
+CF 1 - Simple JS App - Pocket Monsters Catalogue
+This project is an online Pokedex (Pocket Monsters) app that fetches data from the PokeAPI. It displays a list of 150 Pokemon from the API as buttons and allows the user to click each button to find out more about the Pokemon.
+
+Features
+Pokemon Repository: A repository is created from the details of the API that are fetched using Javascript and displayed as button elements.
+
+Pokemon Details: CLicking a Pokemon button opens a modal built with Bootstrap that displays details sucha s the Pokemon's name, height, weight, and image.
+
+JavaScript ES6
+The majority of this app's functionality is written in Javascript ES6, which allows for ease of use on the vast majority of browsers and devices.
+
+Bootstrap
+Bootstrap is used for the display of the modals and the style/responsiveness of the page and its buttons.
+
+PokeAPI
+The PokeAPI is used to fetch the data about the Pokemon. It is a RESTful API that provides data about the Pokemon - their names, heights, weights, images, and more.
+
+Prettier and ESLint
+Prettier is used to format the code correctly and notify of any code style errors in JS, CSS and HTML. ESLint is used to notify of functuonality errors in Javascript.
+
+Other Dependencies
+Dependancies in the app also include the PokeAPI for fetching data about the pokemon and the IIFE where most of the functionality is stored in the Javascript file.
---
+++ dist/dist-script.js
@@ -0,0 +1,15 @@
+const pokemonRepository=(function(){let pokemonList=[];let apiUrl='https://pokeapi.co/api/v2/pokemon/?limit=150';function add(pokemon){if(typeof pokemon==='object'&&'name' in pokemon&&'detailsUrl' in pokemon){pokemonList.push(pokemon)}else{console.log('pokemon is not correct')}}
+function getAll(){return pokemonList}
+function addListItem(pokemon){let listPokemon=document.querySelector('.pokemon-list');let pokemonElement=document.createElement('div');let button=document.createElement('button');button.innerText=pokemon.name;button.classList.add('btn');button.classList.add('list-group-item');button.setAttribute('data-toggle','modal');button.setAttribute('data-target','#pokemonModal');button.addEventListener('click',function(){showDetails(pokemon)});pokemonElement.appendChild(button);listPokemon.appendChild(pokemonElement)}
+function loadList(){return fetch(apiUrl).then(response=>response.json()).then(json=>{json.results.forEach(item=>{let pokemon={name:item.name,detailsUrl:item.url,};add(pokemon)})}).catch(err=>console.error('Error fetching pokemon list: ',err))}
+function loadDetails(item){let url=item.detailsUrl;return fetch(url).then(response=>response.json()).then(details=>{item.imgUrl=details.sprites.front_default;item.height=details.height/10;item.types=details.types;item.weight=details.weight/10;item.abilities=details.abilities}).catch(err=>console.error('Error fetching pokemon details: ',err))}
+function showDetails(item){loadDetails(item).then(()=>{showModal(item)})}
+function showModal(item){let modal=document.querySelector('#pokemonModal');let modalTitle=document.querySelector('.modal-title');let modalBody=document.querySelector('.modal-body');let modalImg=document.querySelector('.modal-img');let closeButton=document.querySelector('.close-button');modalTitle.innerText=item.name;modalBody.innerHTML=`
+      <img class="modal-img" src="${item.imgUrl}" alt="${item.name} image" />
+      <p class="modal-info">Height: ${item.height} </p>
+      <p class="modal-info">Weight: ${item.weight} </p>
+      <ul class="modal-info">Types:</ul>
+    `;for(let i=0;i<item.types.length;i++){let li=document.createElement('li');li.innerText=item.types[i].type.name;modalBody.querySelector('ul').appendChild(li)}
+modalContainer.classList.add('is-visible');closeButton.addEventListener('click',hideModal);window.addEventListener('keydown',(e)=>{if(e.key==='Escape'&&modalContainer.classList.contains('is-visible')){hideModal()}});modal.addEventListener('click',(e)=>{if(e.target===modal){hideModal()}})}
+function hideModal(){let modalContainer=document.querySelector('#modal-container');modalContainer.classList.remove('is-visible')}
+return{add,getAll,addListItem,loadList,loadDetails,showDetails}})();pokemonRepository.loadList().then(()=>{pokemonRepository.getAll().forEach(pokemon=>{pokemonRepository.addListItem(pokemon)})})
